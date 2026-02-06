61e1c35436c8b38290fe110f856a4c1506d95563
Paras Danidhariya <124392753+Parasss111@users.noreply.github.com>
2026-02-06 10:49:25 +0530

    Add margin-top to AdminLayout container

--- src/admin/AdminLayout.jsx
+++ src/admin/AdminLayout.jsx
@@ -2,7 +2,7 @@ import { NavLink, Outlet } from "react-router-dom";
 
 const AdminLayout = () => {
   return (
-    <div className="flex min-h-screen bg-gray-100">
+    <div className="flex min-h-screen bg-gray-100 mt-12">
       {/* ============ SIDEBAR ============ */}
       <aside className="w-64 bg-gradient-to-b from-black to-gray-900 text-white flex flex-col p-6 shadow-xl">
         <h2 className="text-2xl font-extrabold tracking-wide mb-10">
