06e29ab3dbf73545eb5f038b11dd49083bc75cce
Paras Danidhariya <124392753+Parasss111@users.noreply.github.com>
2026-02-06 10:40:15 +0530

    Update AdminOrders.jsx

--- src/admin/AdminOrders.jsx
+++ src/admin/AdminOrders.jsx
@@ -14,9 +14,14 @@ export default function AdminOrders() {
 
       {orders.map(o => (
         <div key={o._id} className="border p-3 mb-2">
-          <p>User: {o.user?.email}</p>
-          <p>Total: ₹{o.totalAmount}</p>
-          <p>Status: {o.status}</p>
+          <p>User: {o.address?.fullName?.email}</p>
+          <p>Street: {o.address?.street}<p/>
+          <p>City: {o.address?.city}<p/>
+          <p>State: {o.address?.state}<p/>
+          <p>Phone: {o.address?.phone}<p/>
+          <p>Total: ₹{o.total}</p>
+          <p>Payment: {o.paymentMethod}</p>
+          <p>Status: {o.orderStatus}</p>
         </div>
       ))}
     </div>
